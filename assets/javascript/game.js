5e30c81f712d43a29632284954c3cece24924a62
Janet Hwu <Janet@MacBook-Pro-2.local>
2018-02-12 03:14:42 -0800

    added basic game functions and variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,85 @@
-// TESTING
-alert("i'm working");
+// GLOBAL CONSTANTS & VARIABLES
+const wordBank = [
+    "aladdin", "alice", "ariel", "aurora", "baloo", "bambi", "belle", "bert", "cinderella", "cruella", "dumbo", "esmeralda", "gaston", "genie", "jafar", "lumiere", "mulan", "pocahontas", "pinocchio", "scar", "sebastian", "tarzan", "timon", "quasimodo", "woody", "ursula"
+]
 
-
-// GLOBAL VARIABLES
-var scoreboard = {
-    wins: 0,
-    losses: 0,
-    wordToGuess: [],
-    answer: [],
-
-}
-
-var validGuesses = [
+const validGuesses = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
 
 // FUNCTIONS
+function newRound() {
+    scoreboard.answerWord = pickNewWord();
+    scoreboard.currentWord = writeBlanks(scoreboard.answerWord);
+    scoreboard.wrongGuesses = "";
+    scoreboard.guessesLeft = 9;
+    updatePage();
+}
+
+function pickNewWord() {
+    var randomIndex = Math.floor(Math.random() * wordBank.length);
+    return wordBank[randomIndex];
+}
+
+function writeBlanks(word) {
+    var blanks = ""
+    for (var i = 0; i < word.length; i++) {
+        blanks += "__ ";
+    }
+    return blanks;
+}
+
+function processGuess(guess) {
+    if (answerWord.includes(guess)) {
+
+    }
+
+}
 
 function updatePage() {
-    document.querySelector("#word-text").innerHTML = "scoreboard.wordToGuess";
-    document.querySelector("#wrong-guesses-text").innerHTML = "test2";
-    document.querySelector("#guesses-left-text").innerHTML = "test3";
-    document.querySelector("#win-counter-text").innerHTML = "test4";
-    document.querySelector("#loss-counter-text").innerHTML = "test5";
+    document.querySelector("#word-text").innerHTML = scoreboard.currentWord;
+    document.querySelector("#wrong-guesses-text").innerHTML = scoreboard.wrongGuesses;
+    document.querySelector("#guesses-left-text").innerHTML = scoreboard.guessesLeft;
+    document.querySelector("#win-counter-text").innerHTML = scoreboard.wins;
+    document.querySelector("#loss-counter-text").innerHTML = scoreboard.losses;
+    logScoreboard(); // test
+}
+
+
+function logScoreboard() {
+    console.log("----- CURRENT GAME ------")
+    for (var i in scoreboard) {
+        if (typeof scoreboard[i] != "function") {
+            console.log(i + ": " + scoreboard[i])
+        }
+    }
+    console.log("-------------------------")
 }
 
 // OBJECTS
+var scoreboard = {
+    currentWord: "",
+    wrongGuesses: "",
+    guessesLeft: 9,
+    wins: 0,
+    losses: 0,
+    answerWord: ""
+}
+
+
+// CALLS
+alert("i'm working"); //test
+newRound();
+
 document.addEventListener("keyup", function(event) {
-    alert(event.key);
+    var userGuess = event.key.toLowerCase();
+    console.log("Key Pressed: " + userGuess);
+    if (validGuesses.includes(userGuess)) {
+        processGuess(userGuess);
+        updatePage(scoreboard);
+    }
 })
 
-
-// CALLS (EVENT LISTENERS)
+// FEATURES TO ADD LATER
+// Songs
+// Names with spaces
